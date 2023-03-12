4483e1851d9a2bed823ee43be6ca4f7b2311744b
Tiago Dias <98658691+tdiascontato@users.noreply.github.com>
2023-03-12 10:49:22 -0300

    Add files via upload

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Olá mundo!');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
